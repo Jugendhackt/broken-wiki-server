1c6e8d326cd40c8b1b9922d3fa45e91909779e5f
Jan <mail@jan-schmidt.de>
2021-10-23 15:57:09 +0200

    commit 23.10.2021

--- scraper/server.py
+++ scraper/server.py
@@ -1,14 +1,17 @@
 import wikipedia_histories
 from flask import Flask
+from flask import request
+from flask import jsonify
 from lib import *
 
-alma_linux = wikipedia_histories.get_history('AlmaLinux')
-print(count_edits_from_users(alma_linux))
+#print(count_edits_from_users(alma_linux))
 app = Flask(__name__)
 
 
 @app.route('/editcount')
-def hello():
-    return count_edits_from_users(alma_linux)
+def editcount():
+    search = request.args.get('search')
+    results = wikipedia_histories.get_history(search)
+    return jsonify(count_edits_from_users(results))
 if __name__ == "__main__":
     app.run()
