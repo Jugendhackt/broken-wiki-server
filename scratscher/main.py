1d6ad164ad9ae51fb7f89ccf8735f77e6a4019a1
Jan <mail@jan-schmidt.de>
2021-10-23 12:08:40 +0200

    added not working scratscher

---
+++ scratscher/main.py
@@ -0,0 +1,10 @@
+import wikipedia_histories
+
+print("1")
+golden_swallow = wikipedia_histories.get_history('Golden swallow')
+print("2")
+golden_swallow
+print("3")
+golden_swallow[16].user
+print("4")
+
