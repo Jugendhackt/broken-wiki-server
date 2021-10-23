d5587144a38ed8384143a8f3a8b904e9106601fa
Jan <mail@jan-schmidt.de>
2021-10-23 17:52:10 +0200

    commit

--- scraper/server.py
+++ scraper/server.py
@@ -2,17 +2,9 @@ import wikipedia_histories
 from flask import Flask
 from flask import request
 from flask import jsonify
-#from flask import render_template
 from lib import *
 
-print("debug getartet")
-#print(count_edits_from_users(alma_linux))
 app = Flask(__name__)
-
-#@app.route("/")
-#def index():
-#    return render_template("results.html") 
-
 
 @app.route('/editcount')
 def editcount():
@@ -20,8 +12,15 @@ def editcount():
     if not search:
         search = "AlmaLinux"
     results = wikipedia_histories.get_history(search)
-    return jsonify(count_edits_from_users(results))
-print("suche gesetzt")
+    return jsonify(edit_count(results))
+
+@app.route('/edittime')
+def edittime():
+    search = request.args.get('search')
+    if not search:
+        search = "AlmaLinux"
+    results = wikipedia_histories.get_history(search)
+    return jsonify(edit_time(results))
 
 @app.after_request
 def add_header(response):
@@ -30,4 +29,3 @@ def add_header(response):
 
 if __name__ == "__main__":
     app.run()
-    print("webside gesterted")
