d03a1a539965ddc8cd06deb28efa8ae54aa639ec
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-10-23 15:16:48 +0200

    used external libraries

--- scraper/server.py
+++ scraper/server.py
@@ -1,14 +1,6 @@
 import wikipedia_histories
 from flask import Flask
-
-def count_edits_from_users(history):
-    return_dict = {}
-    for change in history:
-        if change.user in return_dict.keys():
-            return_dict[change.user] += 1
-        else:
-            return_dict.update({change.user: 1})
-    return return_dict
+from lib import *
 
 alma_linux = wikipedia_histories.get_history('AlmaLinux')
 print(count_edits_from_users(alma_linux))
