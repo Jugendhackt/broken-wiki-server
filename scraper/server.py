e66ca33a12e3e473160c70d3533a680b1c48f423
Jan <mail@jan-schmidt.de>
2021-10-24 10:49:40 +0200

    commit

--- scraper/server.py
+++ scraper/server.py
@@ -2,18 +2,9 @@ import wikipedia_histories
 from flask import Flask
 from flask import request
 from flask import jsonify
-#from flask import render_template
 from lib import *
 
 app = Flask(__name__)
-
-#@app.route("/")
-#def index():
-#    return render_template("index.html") 
-
-#@app.route("/credits")
-#def credits():
-#    return render_template("credits.html") 
 
 @app.route('/editcount')
 def editcount():
@@ -24,9 +15,19 @@ def editcount():
 @app.route('/edittime')
 def edittime():
     questions = request.args.get('q')
-    #if not search:
-    #    search = "AlmaLinux"
     results = wikipedia_histories.get_history(questions, include_text=False)
+    return jsonify(edit_time(results))
+
+@app.route('/editmajor')
+def editmajorcount():
+    questio = request.args.get('q')
+    results = wikipedia_histories.get_history(questio, include_text=False)
+    return jsonify(edit_time(results))
+
+@app.route('/editminor')
+def editminorcount():
+    questi = request.args.get('q')
+    results = wikipedia_histories.get_history(questi, include_text=False)
     return jsonify(edit_time(results))
 
 @app.after_request
