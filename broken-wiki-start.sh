3a7c1a2e523aec71e3270a1b6ecf439ebd3a6ae8
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-10-24 10:59:36 +0200

    added terminal titles

--- broken-wiki-start.sh
+++ broken-wiki-start.sh
@@ -2,5 +2,5 @@
 # This script needs to be placed inside the server repo
 # The frontend repo has to be placed in the same folder as the backend repo
 source venv/bin/activate
-gnome-terminal -- python3 scraper/server.py &
-gnome-terminal -- python3 ../broken-wiki-frontend/start.py &
+gnome-terminal -t "Backen Server" -- python3 scraper/server.py &
+gnome-terminal -t "Frontend Server" -- python3 ../broken-wiki-frontend/start.py &
